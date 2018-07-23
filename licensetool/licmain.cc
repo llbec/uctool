12c94632dc73f2f592c9bbe3372effabc21c2c83
bicliu <klins@163.com>
2018-07-23 22:27:21 +0800

    use db

--- licensetool/licmain.cc
+++ licensetool/licmain.cc
@@ -1,3 +1,5 @@
+#ifdef MYSQL_ENABLE
+
 #include "utils.h"
 #include "MySQLConnection.h"
 #include "util.h"
@@ -11,7 +13,29 @@ int main(int argc, char const *argv[])
 	LoadConfigFile(mapConfigs, mapMultiConfigs);
 
     //ScanTable(GetArg("-sqluser", "root"), GetArg("-sqlpwd", "123456"), GetArg("-sqlthread", 1));
+    MysqlConnectInfo *poolDBInfo = nullptr;
+    {
+        int32_t poolDBPort = GetArg("-dbport", 3306);
+        poolDBInfo = new MysqlConnectInfo(GetArg("-dbhost", "127.0.0.1"), poolDBPort,
+                                            GetArg("-dbuser", "root"),
+                                            GetArg("-dbpwd", "123456"),
+                                            GetArg("-dbname", "udevforums_major_node_bind"));
+    }
+
+    string sql;
+    sql = Strings::Format("select *from udevforums_major_node_bind");
+    MySQLResult res;
+
+    MySQLConnection db(poolDBInfo);
+    for (size_t i = 0; i < 3; i++) {
+      if (db.ping())
+        break;
+    }
+
+    db.query(sql, res);
+    printf("read db %d rows\n", res.numRows());
 
     return 0;
 }
 
+#endif // MYSQL_ENABLE
