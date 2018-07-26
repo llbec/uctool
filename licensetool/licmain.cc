ff85c8e3dc7010f19fb01853be48d1fdfa59c080
bicliu <klins@163.com>
2018-07-26 15:13:12 +0800

    add init chain

--- licensetool/licmain.cc
+++ licensetool/licmain.cc
@@ -7,12 +7,31 @@
 #include "privsend.h"
 
 using namespace std;
+static boost::scoped_ptr<ECCVerifyHandle> global_VerifyHandle;
+
+void InitChain()
+{
+    if(GetBoolArg("-testnet", false))
+	{
+        SelectParams(CBaseChainParams::TESTNET);
+		printf("Info: select TEST net!\n");
+	}
+    else
+	{
+        SelectParams(CBaseChainParams::MAIN);
+		printf("Info: select MAIN net!\n");
+	}
+
+    ECC_Start();
+	global_VerifyHandle.reset(new ECCVerifyHandle());
+}
 
 int main(int argc, char const *argv[])
 {
     /*init*/
     SetFilePath("licensetool.conf");
 	LoadConfigFile(mapArgs, mapMultiArgs);
+    InitChain();
     InitLog(argv);
     CKey ucenterPrivkey;
     CPubKey ucenterPubkey;
