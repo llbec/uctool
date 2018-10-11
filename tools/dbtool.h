2d0e58533ec5cedee08353c559fde1ead65f62cb
bicliu <klins@163.com>
2018-10-11 15:35:53 +0800

    update

--- tools/dbtool.h
+++ tools/dbtool.h
@@ -3,7 +3,7 @@
 #ifdef MYSQL_ENABLE
 void DBCheckNode(int argc, char const * argv[]);
 void DBCheckAll(int argc, char const * argv[]);
-void DBGetAll(int argc, char const * argv[])
+void DBGetAll(int argc, char const * argv[]);
 void DBSelect(int argc, char const * argv[]);
 #endif // MYSQL_ENABLE
 #endif // UTCENTER_DBTOOL_H
