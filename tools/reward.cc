51d3e26cd038712625984bf2ded12dfac9684a47
pazzabec <klins@163.com>
2019-02-25 15:34:23 +0800

    update

--- tools/reward.cc
+++ tools/reward.cc
@@ -2,23 +2,31 @@
 #include "main.h"
 #include "ulord.h"
 
-void GetBlockReward(int height, CAmount & nMiner, CAmount & nBud, CAmount nMN, CAmount & nFud)
+#include <iomanip>
+
+using namespace std;
+
+void GetBlockReward(int height, CAmount & nMiner, CAmount & nBud, CAmount & nMN, CAmount & nFud)
 {
     const Consensus::Params &cp = Params().GetConsensus();
     //Miner
     nMiner = GetMinerSubsidy(height, cp);
 
     //budget&MasterNode
-    if(height >= < cp.nMasternodePaymentsStartBlock) {
-        if(!CSuperblock::IsValidBlockHeight(nBlockHeight)) {
-            //MasterNode
-            nMN = GetMasternodePayment(height);
-            nBud = 0;
-        } else {
+    if(height >= cp.nMasternodePaymentsStartBlock) {
+        if(height >= cp.nSuperblockStartBlock &&
+            ((height % cp.nSuperblockCycle) == 0)) {
             //Budget
             nBud = GetBudget(height, cp);
             nMN = 0;
-        }
+        } else {
+	    //MasterNode
+            nMN = GetMasternodePayment(height);
+            nBud = 0;
+	}
+    } else {
+	nBud = 0;
+	nMN = 0;
     }
 
     //Founder
@@ -38,15 +46,15 @@ void ShowReward(int argc, char const * argv[])
         iYears = atoi(argv[2]);
         iRounds = argc > 3 ? atoi(argv[3]) : 1;
     }
-    int blkminer, blkbud, blkmn, blkfud = 0;
+    CAmount blkminer, blkbud, blkmn, blkfud = 0;
 
     GetBlockReward(iYears, blkminer, blkbud, blkmn, blkfud);
 
     cout << "    height        MinerSubsidy              Budget    MasternodePayment     FoundersReward        BlockSubsidy" << endl;
-	cout << setw(10) << h
-		<< setw(20) << blkminer / COIN
-		<< setw(20) << blkbud / COIN
-		<< setw(20) << blkmn / COIN
-		<< setw(20) << blkfud / COIN
-		<< setw(20) << (blkbud + blkfud + blkminer + blkmn) / COIN << endl;
+	cout << setw(10) << iYears
+		<< setw(20) << blkminer/* / COIN*/
+		<< setw(20) << blkbud/* / COIN*/
+		<< setw(20) << blkmn/* / COIN*/
+		<< setw(20) << blkfud/* / COIN*/
+		<< setw(20) << (blkbud + blkfud + blkminer + blkmn)/* / COIN*/ << endl;
 }
