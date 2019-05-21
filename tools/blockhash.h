14d2494ca66bd427031dd86b07d83d31e9ea7e66
pazzabec <klins@163.com>
2019-05-21 15:53:18 +0800

    update

--- tools/blockhash.h
+++ tools/blockhash.h
@@ -1,6 +1,6 @@
 #ifndef BLOCKHASH_TOOL_H
 #define BLOCKHASH_TOOL_H
 
-void ShowBlockHash(int argc, char const * argv[])
+void ShowBlockHash(int argc, char const * argv[]);
 
 #endif // BLOCKHASH_TOOL_H
